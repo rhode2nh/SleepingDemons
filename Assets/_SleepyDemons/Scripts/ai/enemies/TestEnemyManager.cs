136af8d6b94437881ce4c6bf3800fee6f7730950
Nick <rhodesn10@gmail.com>
2025-03-07 15:48:41 -0500

    added node canvas.

--- Assets/_SleepyDemons/Scripts/ai/enemies/TestEnemyManager.cs
+++ Assets/_SleepyDemons/Scripts/ai/enemies/TestEnemyManager.cs
@@ -2,26 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TestEnemyManager : MonoBehaviour, IDamageable
 {
-    [SerializeField] private float health;
+    [SerializeField] public float health;
     [SerializeField] private float staggerThreshold;
+    [SerializeField] private NavMeshAgent agent;
     
     [SerializeField] public List<GameObject> waypoints;
+    [SerializeField] public GameObject safePoint;
     [SerializeField] public GameObject currentWaypoint;
     [SerializeField] public GameObject lastWaypoint;
+    [SerializeField] public GameObject player;
 
     [SerializeField] private Animator animator;
 
     private float _currentStaggerValue;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     public void TakeDamage(float damage, Vector3 force, Vector3 torque)
     {
         health -= damage;
         _currentStaggerValue += damage;
         if (_currentStaggerValue >= staggerThreshold)
         {
-            animator.SetTrigger("isStaggered");
+            // animator.SetTrigger("isStaggered");
             staggerThreshold = 0.0f;
         }
         
@@ -31,8 +42,27 @@ public class TestEnemyManager : MonoBehaviour, IDamageable
         }
     }
 
+    public void Heal(float amount)
+    {
+        if (health <= 100)
+        {
+            health += amount;
+        }
+
+        if (health > 100)
+        {
+            health = 100;
+        }
+    }
+
+    public void Test()
+    {
+        agent.SetDestination(safePoint.transform.position);
+        Debug.Log(agent.pathPending);
+    }
+
     private void Die()
     {
-        animator.SetTrigger("isDying");       
+        // animator.SetTrigger("isDying");       
     }
 }
